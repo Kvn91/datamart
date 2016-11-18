7054f63c84da939929d28394695d355de0811863
Dev1 ParisInn <Dev1@PARIS-INN.lc>
2016-11-18 18:34:55 +0100

    modif du fichier de deploiement capifony

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,19 +1,16 @@
 set :application, "datamart"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
-set :app_path,    "app"
+set :domain,      "root@ns212345.ovh.net"
+set :deploy_to,   "/home/kevin/datamart"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
-set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
-
-set :model_manager, "doctrine"
-# Or: `propel`
+set :repository,       "git@github.com:Kvn91/datamart.git"
+set :scm,              :git
+set :deploy_via,       :copy
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
+set  :use_sudo,       false
 set  :keep_releases,  3
 
 # Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+logger.level = Logger::MAX_LEVEL
